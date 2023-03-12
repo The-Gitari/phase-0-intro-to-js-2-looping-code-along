109eeb67f8a85d7da154803d07a0e034a16700c1
Noel Gitari <gitarinoel@gmail.com>
2023-03-13 02:24:08 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const writeCards = (arg1,arg2) => {
+
+    const messages = []
+   for (let i = 0; i < arg1.length; i++) {
+       const newMessage = `Thank you, ${arg1[i]}, for the wonderful ${arg2} gift!`;
+
+       messages.push(newMessage);
+     }
+     return messages;
+}
+//second lab 
+
+function countDown(){
+
+let countDown = 10;
+
+while (countDown >= 0) {
+console.log(countDown--);
+}
+}
